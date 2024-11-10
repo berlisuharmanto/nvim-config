1e729d592d8502e0832bb1f1d0c6886fa5bdb9ef
Berli Suharmanto <berli.suharmanto@nusantarainfrastructure.com>
2024-11-10 12:33:43 +0700

    feat: Added support for tailwind

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,10 +16,23 @@ return {
   {
     "olrtg/nvim-emmet",
     config = function()
-      vim.keymap.set({ "n", "v" }, '<leader>xe', require('nvim-emmet').wrap_with_abbreviation)
+      vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
     end,
   },
-
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig",      -- optional
+    },
+    opts = {},
+    config = function()
+      require("tailwind-tools").setup({})
+    end,
+  },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -29,6 +42,7 @@ return {
       lspconfig.intelephense.setup({})
       lspconfig.ts_ls.setup({})
       lspconfig.emmet_language_server.setup({})
+      lspconfig.tailwindcss.setup({})
     end,
   },
 }
