317db1355c9aac6b7b744c91256a2e580a0ea4ef
Berli Suharmanto <berli.suharmanto@nusantarainfrastructure.com>
2024-11-13 15:49:24 +0700

    chore: Disabled codeium

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -37,11 +37,53 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
       lspconfig.lua_ls.setup({})
       lspconfig.eslint.setup({})
       lspconfig.intelephense.setup({})
       lspconfig.ts_ls.setup({})
-      lspconfig.emmet_language_server.setup({})
+      lspconfig.emmet_language_server.setup({
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "php",
+          "blade",
+          "less",
+          "sass",
+          "scss",
+          "pug",
+          "typescriptreact",
+        },
+        -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+        -- **Note:** only the options listed in the table are supported.
+        init_options = {
+          ---@type table<string, string>
+          includeLanguages = {
+            blade = "html"
+          },
+          --- @type string[]
+          excludeLanguages = {},
+          --- @type string[]
+          extensionsPath = {},
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+          preferences = {},
+          --- @type boolean Defaults to `true`
+          showAbbreviationSuggestions = true,
+          --- @type "always" | "never" Defaults to `"always"`
+          showExpandedAbbreviation = "always",
+          --- @type boolean Defaults to `false`
+          showSuggestionsAsSnippets = false,
+          --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+          syntaxProfiles = {},
+          --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+          variables = {},
+        },
+      })
       lspconfig.tailwindcss.setup({})
     end,
   },
