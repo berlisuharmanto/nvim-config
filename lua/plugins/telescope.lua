5a18e9e493ae11c2779c2c7552d56e1b27254183
Berli Suharmanto <berli.suharmanto88@gmail.com>
2025-05-15 20:29:18 +0700

    chore(telescope): Added args to telescope live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,17 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-live-grep-args.nvim",
-		"nvim-telescope/telescope-file-browser.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" }, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-	end,
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" }, {})
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+  end,
 }
